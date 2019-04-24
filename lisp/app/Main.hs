f7a9779dbe2d64af210b10852273f828455a6a7c
nikhilJain17 <nikhil.jain0987@gmail.com>
2019-04-23 22:30:02 -0700

    i guess tree printing works now

--- lisp/app/Main.hs
+++ lisp/app/Main.hs
@@ -6,8 +6,6 @@ import Data.Char
 
 main :: IO ()
 main = putStrLn "use ghci"
--- main = putStrLn (fst ( apply expr " 1 - 2 * 3 + 4 " ) !! 1)
--- main = putStrLn ( convert (isSpace '\n') )
 
 -- This is an example of parsers that parse math expr and evals their result.
 -- Here's the data types we are implicitly using.
@@ -20,25 +18,63 @@ main = putStrLn "use ghci"
 -- addop := + | -
 -- mulop := * | /
 
-expr :: Parser Int
+-----------------------------------------------------------------------------
+-- 1. This code takes in an input and evaluates while parsing.
+-- " 1 + 2 * 3 - 4 " -> [interpretExpr] -> [(-1, "")]
+-----------------------------------------------------------------------------
+interpretExpr :: Parser Int
 addop :: Parser (Int -> Int -> Int)
 mulop :: Parser (Int -> Int -> Int)
 
 -- expr and term are two high level parsers.
 -- recall an expr is a bunch of terms addop-ed...
-expr = term `chainl1` addop 
+interpretExpr = interpretTerm `chainl1` addop 
 -- ...and a term is a bunch of factors mulop-ed
-term = factor `chainl1` mulop 
+interpretTerm = interpretFactor `chainl1` mulop 
 
 -- these parsers are the 'base cases' which do the gritty work.
 -- a factor is a digit or (expression)
-factor = digit +++ do {symb "("; n <- expr; symb ")"; return n}
+interpretFactor = interpretDigit +++ do {symb "("; n <- interpretExpr; symb ")"; return n}
 -- a digit is a number
-digit = do {x <- token (sat isDigit); return (ord x - ord '0')}
+interpretDigit = do {x <- token (sat isDigit); return (ord x - ord '0')}
 
 -- these are just hunting for the operators.
 addop = do {symb "+"; return (+)} +++ do {symb "-"; return (-)}
 mulop = do {symb "*"; return (*)} +++ do {symb "/"; return (div)}
+
+-----------------------------------------------------------------------------
+-- 2. This code takes in an AST and pretty-prints it (aka convert to String).
+-- AST -> [printExpr] -> "1 (2 ( 3 4 5) 4)"
+-----------------------------------------------------------------------------
+-- our tree can be binary since we are only using binary ops
+-- for right now, we assume we have AST String. not worrying about converting types.
+
+data AST a = Nil | Node a (AST a) (AST a) deriving Show
+
+-- learn this fam
+-- https://stackoverflow.com/questions/12556469/nicely-printing-showing-a-binary-tree-in-haskell
+
+-- printExpr :: AST String -> String
+printExpr tree = unlines (printExprHelper tree)
+
+-- printExprHelper :: 
+printExprHelper (Node root left right) 
+    = (show root) : (printSubtree left right)
+        where 
+            printSubtree left right =
+            	((pad "+- " "|  ") (printExprHelper right))
+            	    ++ ((pad "`- " "   ") (printExprHelper left))
+
+            pad first rest = zipWith (++) (first : repeat rest)
+printExprHelper Nil = [] 
+
+
+
+
+
+
+
+
 
 
 
