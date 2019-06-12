db6b81249a026dfdfd3ef4d00f361242e732be1c
nikhilJain17 <nikhil.jain0987@gmail.com>
2019-06-12 08:46:48 -0700

    f algebra in works

--- lisp/app/LispValF.hs
+++ lisp/app/LispValF.hs
@@ -5,16 +5,29 @@ import Datatypes
 
 
 -- f algebra stuff
-data LispValF f = Atom String 
-	| List [f]
-	| DottedList [f] f
-	| Number Integer
-	| String String
-	| Bool Bool
-	| PrimitiveFunc ([f] -> ThrowsError f)
+data LispValF f = AtomF String 
+	| ListF [f]
+	| DottedListF [f] f
+	| NumberF Integer
+	| StringF String
+	| BoolF Bool
+	| PrimitiveFuncF ([f] -> ThrowsError f)
 	-- | Func {params :: [String], vararg :: (Maybe String), body :: [f], closure :: Env}
 
-newtype Fix f = Fx (f (Fix f))
+instance Show (LispValF f) where show = showF
+
+showF :: LispValF f -> String
+showF (AtomF str) = str
+showF (NumberF i) = show i
+showF (StringF str) = str
+showF (BoolF bool) = show bool
+showF (PrimitiveFuncF _) = "<primitive func>"
+showF (ListF l) = "<list> length: " ++ show (length l) -- unwordsListF l
+showF (DottedListF head tail) = "<dotted list>"--unwordsListF head ++ unwordsListF [tail]
+
+-- unwordsListF = unwords . map showF 
+
+newtype Fix f = In (f (Fix f))
 
 type LispValgebra = Fix LispValF
 
